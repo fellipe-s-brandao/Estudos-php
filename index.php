498f84c9df421cd8267b8be7a3f1a99106d890bf
fellipe-s-brandao <fellipesilverio31@hotmail.com>
2021-07-30 14:28:32 -0300

    rotas de paginas

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Notícias .com</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+  <header class="container row">
+    <h2>Notícias .com</h2>
+    <nav class="menu">
+      <ul class="row">
+        <li>
+          <a class="btn" href="index.php">Pagina Inicial</a>
+        </li>
+        <li>
+          <a class="btn" href="index.php?pag=cadastraNoticia">Cadastrar notícia</a>
+        </li>
+        <li>
+          <a class="btn" href="index.php?pag=cadastraCategoria">Cadastrar categoria</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div>
+      <form action="" method="GET">
+          <input type="text" placeholder="Buscar" name="buscar">
+          <input  class="btn btn-busca" type="submit" value="Buscar">
+      </form>
+    </div>
+  </header>
+  
+  <?php
+    $pag = "";
+    if(isset($_GET['pag'])){
+      $pag = addslashes($_GET['pag']);
+    }
+    
+    switch ($pag) {
+      case 'cadastraNoticia':
+        require 'paginas/cadastraNoticia.php';
+        break;
+
+      case 'cadastraCategoria':
+        require 'paginas/cadastraCategoria.php';
+        break;
+
+      default:
+        require 'paginas/home.php';
+        break;
+    }
+  ?>
+</body>
+</html>
--- paginas/cadastraCategoria.php
+++ paginas/cadastraCategoria.php
@@ -4,9 +4,13 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tela de login</title>
+  <title>Notícias .com</title>
+  <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
+  <main class= "container">
+
+  </main>
   
 </body>
 </html>
