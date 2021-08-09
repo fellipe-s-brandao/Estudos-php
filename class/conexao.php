dae023ffbaa3b39ea429e72eb0cfccfc5f43df1a
fellipe-s-brandao <fellipesilverio31@hotmail.com>
2021-08-09 08:58:37 -0300

    pronto

--- class/conexao.php
+++ class/conexao.php
@@ -1,16 +1,12 @@
 <?php
 
-
-Class Conexao{
+class Conexao{
     public function conectar($nome,$host,$usuario,$senha){
         global $pdo;
         try{
             $pdo = new PDO("mysql:dbname=".$nome.";host=".$host,$usuario,$senha);
         }catch(PDOException $e){
-            echo $e->getMensage();
+            echo $e->getMessage();
         }
     }
 }
-
-
-?>
